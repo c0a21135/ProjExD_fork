1cb2ba06bee8331f5daec493fda0496e493e0124
yajima <c0a21129af@edu.teu.ac.jp>
2022-11-29 15:56:00 +0900

    配置

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,53 +2,74 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
-def button_click(num):
+def button_click(event):
+    btn=event.widget
+    num=btn["text"]
     if num=="=":
-        calc()
+        formula=input_text.get()
+        input_text.delete(0,tk.END)
+        try:
+            formula=formula.replace("×","*")
+            formula=formula.replace("÷","/")
+            ans=eval(formula)
+            input_text.insert(tk.END,str(ans))
+        except:
+            input_text.insert(tk.END,"エラー")
+    elif num=="BS":
+        formula=input_text.get()
+        input_text.delete(0,tk.END)
+        input_text.insert(tk.END,formula[:-1])
+    elif num=="C":
+        input_text.delete(0,tk.END)
     else:
-        input_text.insert(tk.END,str(num))
-    tkm.showinfo("押すな",f"{num}のボタンが押されました。")
+        if num=="+" or num=="÷" or num=="×":
+            try:
+                formula=input_text.get()[-1]
+            except:
+                formula=""
+            if formula in ["+","-","×","÷",""]:
+                pass
+            else:
+                input_text.insert(tk.END,(num))
+        elif num=="-":
+            try:
+                formula=input_text.get()[-1]
+            except:
+                formula=""
+            if formula=="-":
+                pass
+            else:
+                input_text.insert(tk.END,num)    
+        else:
+            input_text.insert(tk.END,num)
 
-def calc():
-    formula=input_text.get()
-    formula=formula.split("+")
-    input_text.delete(0,tk.END)
-    try:
-        formula=[int(i) for i in formula]
-        input_text.insert(tk.END,str(sum(formula)))
-    except Exception:
-        input_text.insert(tk.END,"エラー")
+
+def chenge_bg(event):
+    event.widget["bg"]="#87CEEB"
+def back_bg(event):
+    event.widget["bg"]="SystemButtonFace"
 
 
 
 root=tk.Tk()
 root.title("calculate")
 
-input_text=tk.Entry(width=10,font=("",40),justify=tk.RIGHT)
-input_text.grid(row=0,column=0,columnspan=3)
-button_9=tk.Button(root,text="9",font=("",30),width=4,height=2,command=lambda:button_click(9))
-button_9.grid(row=1,column=0)
-button_8=tk.Button(root,text="8",font=("",30),width=4,height=2,command=lambda:button_click(8))
-button_8.grid(row=1,column=1)
-button_7=tk.Button(root,text="7",font=("",30),width=4,height=2,command=lambda:button_click(7))
-button_7.grid(row=1,column=2)
-button_6=tk.Button(root,text="6",font=("",30),width=4,height=2,command=lambda:button_click(6))
-button_6.grid(row=2,column=0)
-button_5=tk.Button(root,text="5",font=("",30),width=4,height=2,command=lambda:button_click(5))
-button_5.grid(row=2,column=1)
-button_4=tk.Button(root,text="4",font=("",30),width=4,height=2,command=lambda:button_click(4))
-button_4.grid(row=2,column=2)
-button_3=tk.Button(root,text="3",font=("",30),width=4,height=2,command=lambda:button_click(3))
-button_3.grid(row=3,column=0)
-button_2=tk.Button(root,text="2",font=("",30),width=4,height=2,command=lambda:button_click(2))
-button_2.grid(row=3,column=1)
-button_1=tk.Button(root,text="1",font=("",30),width=4,height=2,command=lambda:button_click(1))
-button_1.grid(row=3,column=2)
-button_0=tk.Button(root,text="0",font=("",30),width=4,height=2,command=lambda:button_click(0))
-button_0.grid(row=4,column=0)
-button_p=tk.Button(root,text="+",font=("",30),width=4,height=2,command=lambda:button_click("+"))
-button_p.grid(row=4,column=1)
-button_e=tk.Button(root,text="=",font=("",30),width=4,height=2,command=lambda:button_click("="))
-button_e.grid(row=4,column=2)
+input_text=tk.Entry(width=12,font=("",40),justify=tk.RIGHT)
+input_text.grid(row=0,column=0,columnspan=4)
 
+button_labels=[["+","-","×","÷","="],["1/x","C","BS"],["+/-","0","."]]
+for i in range(20):
+    if i%4==3:
+        bl=button_labels[0][i//4]
+    elif i//4==0:
+        bl=button_labels[1][i%4]
+    elif i//4==4:
+        bl=button_labels[2][i%4]
+    else:
+        bl=(4-i//4)*3-i%4
+    button=tk.Button(root,text=str(bl),font=("",30),width=4,height=2)
+    button.bind("<1>",button_click)
+    button.bind("<Enter>",chenge_bg)
+    button.bind("<Leave>",back_bg)    
+    button.grid(row=1+i//4,column=i%4)
 root.mainloop()
