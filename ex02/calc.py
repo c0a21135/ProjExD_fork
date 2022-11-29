f6f2b46b4896aa6093fb4d0730a8bcaee9d7fd5c
yajima <c0a21129af@edu.teu.ac.jp>
2022-11-29 17:46:08 +0900

    issueuに対応しました。#4,#5

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
 import math
 
 
@@ -79,26 +78,26 @@ def back_bg(event):
     event.widget["bg"]="SystemButtonFace"
 
 
+if __name__=="__main__":
+    root=tk.Tk()
+    root.title("calculate")
 
-root=tk.Tk()
-root.title("calculate")
+    input_text=tk.Entry(width=12,font=("",40),justify=tk.RIGHT)
+    input_text.grid(row=0,column=0,columnspan=4)
 
-input_text=tk.Entry(width=12,font=("",40),justify=tk.RIGHT)
-input_text.grid(row=0,column=0,columnspan=4)
-
-button_labels=[["+","-","×","÷","="],["1/x","C","BS"],["x!","0","."]]
-for i in range(20):
-    if i%4==3:
-        bl=button_labels[0][i//4]
-    elif i//4==0:
-        bl=button_labels[1][i%4]
-    elif i//4==4:
-        bl=button_labels[2][i%4]
-    else:
-        bl=(4-i//4)*3-i%4
-    button=tk.Button(root,text=str(bl),font=("",30),width=4,height=2)
-    button.bind("<1>",button_click)
-    button.bind("<Enter>",chenge_bg)
-    button.bind("<Leave>",back_bg)    
-    button.grid(row=1+i//4,column=i%4)
-root.mainloop()
+    button_labels=[["+","-","×","÷","="],["1/x","C","BS"],["x!","0","."]]
+    for i in range(20):
+        if i%4==3:
+            bl=button_labels[0][i//4]
+        elif i//4==0:
+            bl=button_labels[1][i%4]
+        elif i//4==4:
+            bl=button_labels[2][i%4]
+        else:
+            bl=(4-i//4)*3-i%4
+        button=tk.Button(root,text=str(bl),font=("",30),width=4,height=2)
+        button.bind("<1>",button_click)
+        button.bind("<Enter>",chenge_bg)
+        button.bind("<Leave>",back_bg)    
+        button.grid(row=1+i//4,column=i%4)
+    root.mainloop()
