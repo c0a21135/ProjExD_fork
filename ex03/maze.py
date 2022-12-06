4054f434cf9c1d2eabcfeeee4d9fc70ed28bb9d2
yajima <c0a21129af@edu.teu.ac.jp>
2022-12-06 14:25:01 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,23 +11,25 @@ def key_up(event):
     key=""
 
 def main_proc():
-    global cx, cy
-    if key=="Up":
-        cy-=20
-    elif key=="Down":
-        cy+=20
-    elif key=="Left":
-        cx-=20
-    elif key=="Right":
-        cx+=20
+    global mx, my
+    if key=="Up" and maze[mx][my-1]==0:
+        my-=1
+    elif key=="Down" and maze[mx][my+1]==0:
+        my+=1
+    elif key=="Left" and maze[mx-1][my]==0:
+        mx-=1
+    elif key=="Right" and maze[mx+1][my]==0:
+        mx+=1
+    cx=mx*100+50
+    cy=my*100+50
     canvas.coords("koukaton_1",cx,cy)
     root.after(300,main_proc)
 
 
 
 if __name__=="__main__":
-    cx=150
-    cy=150
+    mx=1
+    my=1
     key=""
     maze=make_maze(15,9)
 
@@ -37,8 +39,9 @@ if __name__=="__main__":
     canvas.pack()
     show_maze(canvas,maze)
     koukaton=tk.PhotoImage(file="./fig/0.png")
-    canvas.create_image(cx,cy,image=koukaton,tag="koukaton_1")
+    canvas.create_image(mx*100+50,mx*100+50,image=koukaton,tag="koukaton_1")
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
     main_proc()
+    print(maze)
     root.mainloop()
