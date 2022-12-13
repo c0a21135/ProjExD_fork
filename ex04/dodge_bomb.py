5414bd4cb7256b0cf6314dfac9cca29f6be433a0
yajima <c0a21129af@edu.teu.ac.jp>
2022-12-13 15:57:41 +0900

    8まで

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,7 +11,7 @@ def main():
     scrn_sfc = pg.display.set_mode((WIDTH, HEIGHT))
 
     clock = pg.time.Clock()
-    clock.tick(1000)
+    clock.tick(10)
 
     bg_sfc = pg.image.load("./fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
@@ -28,11 +28,12 @@ def main():
         draw_sfc = pg.Surface((20, 20))
         draw_sfc.set_colorkey((0,0,0))
         pg.draw.circle(draw_sfc,(255,0,0),(10, 10),10)
-        bomb_sfc_lst[i] = [draw_sfc,
-                           [random.randint(10,WIDTH-10),random.randint(50,HEIGHT-50)]]
+        bomb_rct = draw_sfc.get_rect()
+        bomb_rct.center= random.randint(10,WIDTH-10), random.randint(10,HEIGHT-10)
+        bomb_sfc_lst[i] = [draw_sfc,bomb_rct,[vx,vy]]
     print(bomb_sfc_lst)
     for bomb_sfc in bomb_sfc_lst:
-        scrn_sfc.blit(bomb_sfc[0], tuple(bomb_sfc[1]))
+        scrn_sfc.blit(bomb_sfc[0], bomb_sfc[1])
 
     while True:
         pg.display.update()
@@ -41,19 +42,31 @@ def main():
         scrn_sfc.blit(bg_sfc, bg_rct)
         scrn_sfc.blit(tori_sfc, tori_rct)
         for bomb_sfc in bomb_sfc_lst:
-            bomb_sfc[1]=[bomb_sfc[1][0]+vx, bomb_sfc[1][1]+vy]    
-            scrn_sfc.blit(bomb_sfc[0], tuple(bomb_sfc[1]))
+            if 0>bomb_sfc[1].x+bomb_sfc[2][0] or bomb_sfc[1].x+bomb_sfc[2][0]>WIDTH:
+                print(bomb_sfc[1].x,bomb_sfc[2][0])
+                bomb_sfc[2][0]*=-1
+            # else:
+            #     bomb_sfc[2][0]+=1
+            if 0>bomb_sfc[1].y+bomb_sfc[2][1] or bomb_sfc[1].y+bomb_sfc[2][1]>HEIGHT:
+                bomb_sfc[2][1]*=-1
+            # else:
+            #     bomb_sfc[2][1]+=1
+            bomb_sfc[1].move_ip(bomb_sfc[2][0], bomb_sfc[2][1])
+            scrn_sfc.blit(bomb_sfc[0], bomb_sfc[1])
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            if pressed[pg.K_UP]:
+            if pressed[pg.K_UP] and tori_rct.y-1>=0:
                 tori_rct.move_ip(0, -1)
-            if pressed[pg.K_DOWN]:
+            if pressed[pg.K_DOWN] and tori_rct.y+1<=HEIGHT:
                 tori_rct.move_ip(0, 1)
-            if pressed[pg.K_LEFT]:
+            if pressed[pg.K_LEFT] and tori_rct.x-1>=0:
                 tori_rct.move_ip(-1, 0)
-            if pressed[pg.K_RIGHT]:
+            if pressed[pg.K_RIGHT] and tori_rct.x+1<=WIDTH:
                 tori_rct.move_ip(1, 0)
+        for bomb_fanc in bomb_sfc_lst:
+            if tori_rct.colliderect(bomb_fanc[1]):
+                return
             
 
 if __name__ == "__main__":
